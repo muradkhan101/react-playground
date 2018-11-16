0d5928ba7b602f6123b4a862fa5eedeaef04693b
Murad Khan <mkhan@sbmcorp.com>
2018-11-16 07:37:55 -0800

    Updated webpack configs and folder structure

--- src/other.tsx
+++ src/other.tsx
@@ -6,17 +6,22 @@ import { Link } from 'react-router-dom';
 import { RouterComponent } from './Routing/Router';
 import { RouteChildComponent } from './Routing/RouteChild';
 
-
 const App = () => (
     <RouterComponent>
         <React.Fragment>
-            <RouteChildComponent color={'blue'} link={'/potato'} >
-                <Link to={'./deep'}>Go Deep</Link>
+            <RouteChildComponent color={'blue'} link={'/potato'}>
+                <Link to="./deep">Go Deep</Link>
                 <Link to={'../'}>Nested Up One</Link>
-                <RouteChildComponent color={'orange'} link={'deep'} >1</RouteChildComponent>
+                <RouteChildComponent color={'orange'} link={'deep'}>
+                    1
+                </RouteChildComponent>
             </RouteChildComponent>
-            <RouteChildComponent color={'green'} link={'/grass'} >2</RouteChildComponent>
-            <RouteChildComponent color={'orange'} link={'/orange'} >1</RouteChildComponent>
+            <RouteChildComponent color={'green'} link={'/grass'}>
+                2
+            </RouteChildComponent>
+            <RouteChildComponent color={'orange'} link={'/orange'}>
+                1
+            </RouteChildComponent>
             <Link to={'/potato'}>Potato</Link>
             <Link to={'/grass'}>Grass</Link>
             <Link to={'/orange'}>Orange</Link>
